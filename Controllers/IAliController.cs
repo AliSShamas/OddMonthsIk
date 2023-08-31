106e18e1dcc6ee2c6d08e2544b48c06d065ee01f
Ali Shamas <127535679+zZSlinkerZz@users.noreply.github.com>
2023-08-31 15:28:39 +0300

    First Commit

---
+++ Controllers/IAliController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AliServiceController : ControllerBase
+{
+    private readonly IAliService _AliService;
+    public AliServiceController(IAliService AliService)
+    {
+        _AliService = AliService;  
+    }
+    [HttpGet]
+    [Route("OddMonths")]
+    public ActionResult <List<string>> oddMonths()
+    {
+    List<string> oddMonth = _AliService.GetOddMonths();
+        return Ok(oddMonth);
+    }
+     
+}
---
+++ IAliService.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+
+public interface IAliService
+{
+    List<string> GetOddMonths();
+}
+
+public class Ali: IAliService
+{
+    public List<string>GetOddMonths()
+    { 
+        List<string> oddMonths = new List<string>(); 
+        for(int month = 1;month<=12;month+=2)
+        {
+            oddMonths.Add(CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(month));
+        }
+        return oddMonths;
+    }
+}
